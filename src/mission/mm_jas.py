d518eb1bc3ab804b3ee57196426e262257afb176
Thomas Prime <thopri@NOCS06432C.local>
2024-09-05 07:26:09 +0100

    intake now seems to work?

--- src/mission/mm_jas.py
+++ src/mission/mm_jas.py
@@ -1,13 +1,13 @@
 import s3fs
 import xarray as xr
+import intake
 
+# s3 = s3fs.S3FileSystem(endpoint_url="https://noc-msm-o.s3-ext.jc.rl.ac.uk",anon=True)
+# s3_store = s3fs.S3Map('s3://mamma-mia/eORCA12_201908',s3=s3)
 
-s3 = s3fs.S3FileSystem(endpoint_url="https://noc-msm-o.s3-ext.jc.rl.ac.uk",anon=True)
-s3_store = s3fs.S3Map('s3://mamma-mia/eORCA12_201908',s3=s3)
-
-#cat = intake.open_catalog('src/mission/catalog.yml')
-#data = cat.eORCA12_201908.to_dask()
-data = xr.open_zarr(s3_store)
+cat = intake.open_catalog('src/mission/catalog.yml')
+data = cat.eORCA12_201908.to_dask()
+#data = xr.open_zarr(s3_store)
 
 print(data)
 print("the end")
