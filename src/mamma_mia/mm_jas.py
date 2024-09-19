4be89fcdccd20296ac0698f709e5be0cc65f97c6
Thomas Prime <thopri@NOCS06432C.local>
2024-09-19 09:47:55 +0100

    added search term to catalog search example

--- src/mamma_mia/mm_jas.py
+++ src/mamma_mia/mm_jas.py
@@ -7,8 +7,9 @@ catalog = intake.open_catalog('catalog.yml')
 
 # Define your search criteria
 search_bbox = [-50, -30, 50, 30]  # A region with longitude/latitude bounds
-search_start_time = datetime(2019, 6, 1,tzinfo=timezone.utc)
-search_end_time = datetime(2019, 6, 30,tzinfo=timezone.utc)
+search_start_time = datetime(2019, 1, 1,tzinfo=timezone.utc)
+search_end_time = datetime(2019, 1, 30,tzinfo=timezone.utc)
+search_term = "temperature"
 
 # Search through the catalog based on spatial and temporal extent
 search_results = []
@@ -18,6 +19,7 @@ for entry_name, entry in catalog.items():
     # Get the spatial and temporal extents from metadata
     spatial_extent = metadata.get('spatial_extent', [])
     temporal_extent = metadata.get('temporal_extent', [])
+    aliases = metadata.get('aliases', [])
 
     # Convert temporal extent strings to datetime objects for comparison
     if temporal_extent:
@@ -28,7 +30,8 @@ for entry_name, entry in catalog.items():
         if (spatial_extent and
                 search_bbox[0] >= spatial_extent[0] and search_bbox[2] <= spatial_extent[2] and
                 search_bbox[1] >= spatial_extent[1] and search_bbox[3] <= spatial_extent[3] and
-                search_start_time >= start_datetime and search_end_time <= end_datetime):
+                search_start_time >= start_datetime and search_end_time <= end_datetime and
+                search_term in aliases):
             search_results.append(entry)
 
 # Print the search results
@@ -37,16 +40,18 @@ if search_results:
     for result in search_results:
         print(f"- {result.name}")
         data = result.to_dask()
+        var = result.describe()['metadata']["variable"]
+        print(data)
 else:
     print("No datasets found with the specified spatial/temporal criteria.")
 
 
-cropped_data = data.sel(y=slice(2500,3000), x=slice(3250,3750),deptht=slice(5,200),time_counter="2019-07-16")
+cropped_data = data.sel(y=slice(2500,3000), x=slice(3250,3750),deptht=slice(5,200),time_counter="2019-01-16")
 
 #data = xr.open_zarr(s3_store)
 #
 print(cropped_data)
-temp = cropped_data.thetao
+temp = cropped_data[var]
 temp = temp.sel(deptht=5,method='nearest')
 temp.plot()
 print(temp)
