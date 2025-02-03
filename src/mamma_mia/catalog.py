938e51805a5b44456f41ded3b2e846429af2d3b5
Thomas Prime <thopri@laptop-i9hpvg63.home>
2025-02-03 11:07:15 +0000

    updated env with pinned versions and set cache default to False

--- src/mamma_mia/catalog.py
+++ src/mamma_mia/catalog.py
@@ -35,7 +35,7 @@ class Cats:
     msm_cat: intake.Catalog = field(init=False)
     search : InitVar[str] = "Global"
     cat_path: InitVar[str] = "https://noc-msm-o.s3-ext.jc.rl.ac.uk/mamma-mia/catalog/catalog.yml"
-    overwrite: bool = False
+    overwrite: bool = True
     # TODO need some kind of refresh option that will delete caches of downloaded data. (user enabled and probably if data is older than x?)
     def __post_init__(self, search,cat_path ):
         self.cmems_cat = copernicusmarine.describe(contains=[search], include_datasets=True,overwrite_metadata_cache=self.overwrite)
