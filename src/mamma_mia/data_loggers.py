dd7cd838287c309747e886d377656429fbb767d6
thopri <thopri@noc.ac.uk>
2025-03-07 08:31:43 +0000

    more progress! probably?

--- src/mamma_mia/data_loggers.py
+++ src/mamma_mia/data_loggers.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
-from mamma_mia.sensors import Sensor2
+from mamma_mia.sensors import Sensor
 
 @dataclass
-class ALRDataLogger(Sensor2):
+class ALRDataLogger(Sensor):
     bodc_sensor_model_id: int = field(default=761,init=False)
     bodc_sensor_model_registry_id: int = field(default=281,init=False)
     instrument_type: str = field(default="data loggers",init=False)
@@ -15,7 +15,7 @@ class ALRDataLogger(Sensor2):
         #self.parameters = {"Latitude":ALR_Latitude}
 
 @dataclass
-class SlocumDataLogger(Sensor2):
+class SlocumDataLogger(Sensor):
     bodc_sensor_model_id: int = field(default=450,init=False)
     bodc_sensor_model_registry_id: int = field(default=236,init=False)
     instrument_type: str = field(default="data loggers",init=False)
@@ -26,23 +26,3 @@ class SlocumDataLogger(Sensor2):
     def __post_init__(self):
         pass
 
-Teledyne_Slocum_G1_G2_unit_398 = SlocumDataLogger(
-    bodc_sensor_version_id=1139,
-    bodc_sensor_id=686,
-    sensor_name="Teledyne Slocum G1+G2 unit_398",
-    sensor_serial_number="unit_398"
-)
-
-ALR_v1_data_logger_ALR_4 = ALRDataLogger(
-    bodc_sensor_version_id=1296,
-    bodc_sensor_id=838,
-    sensor_name="ALR v1 data logger ALR_4",
-    sensor_serial_number="ALR_4",
-)
-
-ALR_v2_data_logger_ALR_6 = ALRDataLogger(
-    bodc_sensor_version_id=1664,
-    bodc_sensor_id=1232,
-    sensor_name="ALR data logger v2 ALR_6",
-    sensor_serial_number="ALR_6"
-)
