a2318bc5b004626772f725c771681f425d01b2ae
thopri <thopri@noc.ac.uk>
2025-05-15 09:20:03 +0100

    modified cat init so it only loads catalogs once.

--- src/mamma_mia/catalog.py
+++ src/mamma_mia/catalog.py
@@ -18,17 +18,19 @@ class Cats:
     Returns:
         Populated Cats object
     """
-    cmems_cat: dict = field(factory=dict)
-    msm_cat: intake.Catalog = field(factory=intake.Catalog)
+    cmems_cat: dict = None
+    msm_cat: intake.Catalog = None
     search: str = "Global"
     cat_path: str = "https://noc-msm-o.s3-ext.jc.rl.ac.uk/mamma-mia/catalog/catalog.yml"
     overwrite: bool = False
     sources: dict = {"CMEMS": 1, "MSM": 2}
 
     def init_catalog(self):
-        self.cmems_cat = copernicusmarine.describe(contains=[], include_datasets=True,
-                                                   overwrite_metadata_cache=self.overwrite)
-        self.msm_cat = intake.open_catalog(self.cat_path)
+        if self.cmems_cat is None:
+            self.cmems_cat = copernicusmarine.describe(contains=[], include_datasets=True,
+                                                       overwrite_metadata_cache=self.overwrite)
+        if self.msm_cat is None:
+            self.msm_cat = intake.open_catalog(self.cat_path)
 
     def get_sources_list(self):
         """
