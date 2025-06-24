10b268be385131f64f3dc01b94a0ba9b97b5d6cf
thopri <thopri@noc.ac.uk>
2025-06-24 14:30:39 +0100

    tried some options for keplerGL export

--- sandbox/to_KeplerGL.py
+++ sandbox/to_KeplerGL.py
@@ -1,33 +1,48 @@
 import csv
+
+import geojson
 import zarr
 import numpy as np
+from shapely.geometry.geo import mapping
 
-ds = zarr.open("../BIO-Carbon.zarr/Deployment_649/payload")
-lat = ds["ALATPT01"][:]
-lon = ds["ALONPT01"][:]
-depth = ds["ADEPPT01"][:] * -1 # the -1 is to invert the depths so they plot the "right" way round in kepler.gl
+# stride = 180
+
+ds = zarr.open("../RAPID_array_virtual_mooring.zarr/RAD24_01/payload")
+lat = ds["LATITUDE"][:]
+lon = ds["LONGITUDE"][:]
+depth = ds["GLIDER_DEPTH"][:] * -1 # the -1 is to invert the depths so they plot the "right" way round in kepler.gl
 time = ds["TIME"][:]
 temp = ds["INSITU_TEMPERATURE"][:]
 sal = ds["PRACTICAL_SALINITY"][:]
-chlor = ds["CHLOROPHYLL"][:]
+#chlor = ds["CHLOROPHYLL"][:]
 time = (time/1e9).astype("datetime64[s]").astype(int)
+
+# # stride arrays (resolution too high for animations)
+# lat = lat[::stride]
+# lon = lon[::stride]
+# depth = depth[::stride]
+# time = time[::stride]
+# chlor = chlor[::stride]
+# heading = heading[::stride]
+# temp = temp[::stride]
+# sal = sal[::stride]
 
 # mask based on null temps
 mask = ~np.isnan(temp)
 temp = temp[mask]
 sal = sal[mask]
-chlor = chlor[mask]
+#chlor = chlor[mask]
 lat = lat[mask]
 lon = lon[mask]
 depth = depth[mask]
 time = time[mask]
 
-file_name = "BIO_CARBON_649.csv"
+file_name = "RAPID_MOORING2.csv"
 with open(file_name, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     # Write the header
-    csvwriter.writerow(['Latitude', 'Longitude', 'Depth','Time','Temperature','Salinity','Chlorophyll'])
+    csvwriter.writerow(['Latitude', 'Longitude', 'Depth','Time','Temperature','Salinity'])#,'Chlorophyll'])
 
     # Generate rows following the pattern
     for i in range(time.__len__()):
@@ -37,10 +52,38 @@ with open(file_name, 'w', newline='') as csvfile:
         t = time[i]
         t2 = temp[i]
         s = sal[i]
-        c = chlor[i]
+        #c = chlor[i]
 
-        csvwriter.writerow([y,x,z,t,t2,s,c])
+        csvwriter.writerow([y,x,z,t,t2,s])#,c])
 
+# from shapely.geometry import Polygon
+# import math
+#
+# def make_arrow(lat, lon, heading_deg, length=0.001):
+#     angle = math.radians(heading_deg)
+#     dx = length * math.cos(angle)
+#     dy = length * math.sin(angle)
+#
+#     # Arrow triangle (tip, back left, back right)
+#     tip = (lon + dx, lat + dy)
+#     base_left = (lon - dy * 0.2, lat + dx * 0.2)
+#     base_right = (lon + dy * 0.2, lat - dx * 0.2)
+#
+#     return Polygon([tip, base_left, base_right, tip])
+#
+#
+# features = []
+#
+# for i in range(time.__len__()):
+#     arrow = make_arrow(lat[i], lon[i], heading[i])
+#     feature = geojson.Feature(geometry=mapping(arrow), properties={"timestamp": int(time[i]), "latitude": lat[i], "longitude": lon[i]})
+#     features.append(feature)
+#
+# feature_collection = geojson.FeatureCollection(features)
+#
+# with open("BIO_CARBON_649.geojson", "w") as f:
+#     geojson.dump(feature_collection, f, indent=2)
+#
 
 
 # # Make sure all arrays are the same length
