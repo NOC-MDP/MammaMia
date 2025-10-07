3940e0520203b1231523e346527b561266bd87f5
thopri <thopri@noc.ac.uk>
2025-10-07 20:02:35 +0100

    made a start on standardising doc strings for classes and functions/methods

--- src/mamma_mia/catalog.py
+++ src/mamma_mia/catalog.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from attrs import define, field
+from attrs import define
 from OceanDataStore import OceanDataCatalog
 import copernicusmarine
 from loguru import logger
@@ -21,22 +21,32 @@ class Cats:
     Catalog class, contains all the model source data that is available to download. There is a field for each source,
     these are populated with their relevant catalogs that can be searched for matching worlds.
 
-    Args:
-        search: string that overrides the default search term used for CMEMS sources
-        cat_path: string that overrides the default catalog location used for MSM sources
-        overwrite: bool that overwrites the metadata cache used for CMEMS sources
+    Parameters
+    ----------
+    search: str, optional
+        overrides the default search term used for CMEMS sources
+    overwrite: bool, optional
+        overwrites the metadata cache used for CMEMS sources
 
-    Returns:
-        Populated Cats object
+    Attributes
+    ----------
+    cmems_cat: CopernicusMarineCatalog
+        cmems catalog class
+    msm_cat: OceanDataCatalog
+        msm catalog class
+    sources: dict
+        dictionary containing integers that denote a model source priority
     """
     cmems_cat: copernicusmarine.CopernicusMarineCatalogue = None
     msm_cat: OceanDataCatalog = None
     search: str = "Global"
-    cat_path: str = "https://noc-msm-o.s3-ext.jc.rl.ac.uk/mamma-mia/catalog/catalog.yml"
     overwrite: bool = False
     sources: dict = {"CMEMS": 1, "MSM": 2}
 
     def init_catalog(self):
+        """
+        initialize the catalog class
+        """
         if self.cmems_cat is None:
             self.cmems_cat = copernicusmarine.describe(contains=[])
         if self.msm_cat is None:
@@ -45,7 +55,6 @@ class Cats:
     def get_sources_list(self):
         """
         list of model sources that can be downloaded to use within Mamma Mia along with their priority
-        Returns: list of available world sources
 
         """
         return self.sources
@@ -53,8 +62,12 @@ class Cats:
     def set_priority(self, source: str, priority: int):
         """
         Sets the priority of a given source
-        Returns:
-
+        Parameters
+        ----------
+        source: str
+            source to update
+        priority: int
+            priority of the source (higher has higher priority)
         """
         self.sources[source] = priority
         logger.success(f"Set priority of {source} to {priority}")
